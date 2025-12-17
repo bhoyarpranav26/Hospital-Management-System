1f947f418ddd5db28488f53c0355eb89e7317e98
bhoyarpranav26 <bhoyarpranav26@gmail.com>
2025-12-17 11:24:58 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,2 +1,13 @@
-#!/bin/bash
-hospitalManagementSystem/mvnw spring-boot:run
+#!/bin/sh
+set -e
+
+echo "Moving into Spring Boot project directory"
+cd hospitalManagementSystem
+
+chmod +x mvnw
+
+echo "Building application"
+./mvnw clean package -DskipTests
+
+echo "Starting application"
+java -jar target/*.jar
